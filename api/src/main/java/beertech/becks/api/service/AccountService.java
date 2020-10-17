c498683463d9c511d1afff91f60a78ccc28be842
Marcilio <Marcilio.Souza@ab-inbev.com>
2020-10-17 10:21:31 -0300

    Account list all

--- api/src/main/java/beertech/becks/api/service/AccountService.java
+++ api/src/main/java/beertech/becks/api/service/AccountService.java
@@ -6,9 +6,13 @@ import beertech.becks.api.exception.account.AccountDoesNotExistsException;
 import beertech.becks.api.tos.request.AccountRequestTO;
 import beertech.becks.api.tos.response.BalanceResponseTO;
 
+import java.util.List;
+
 public interface AccountService {
 
 	Account createAccount(AccountRequestTO accountTO) throws AccountAlreadyExistsException;
+
+	List<Account> getAll();
 
 	BalanceResponseTO getBalance(String accountCode) throws AccountDoesNotExistsException;
 }
