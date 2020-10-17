7450ea355cdb2ef6984eadc071be5fc3728dbfbd
Victor Carvalho <victorhrgc@gmail.com>
2020-10-17 10:40:16 -0300

    Fixes to the flyway script

--- api/src/main/resources/db/migration/V3__adding_user_and_roles.sql
+++ api/src/main/resources/db/migration/V3__adding_user_and_roles.sql
@@ -1,17 +1,17 @@
-truncate table transaction;
-truncate table account;
+delete from transaction where id > 1;
+delete from account where id > 1;
 
-create table USER (
+create table users (
     ID numeric(20) NOT NULL,
     DOCUMENT_NUMBER varchar(20) NOT NULL,
     EMAIL varchar(200) NOT NULL,
     ROLE int NOT NULL
 );
 
-alter table "user" add constraint pk_user primary key(ID);
+alter table users add constraint pk_user primary key(ID);
 
 
 alter table account add column FK_USER_ID numeric(20) not null;
 
 alter table account add constraint fk_account_user foreign key (FK_USER_ID)
-references user (ID) on delete no action on update no action;
+references users (ID) on delete no action on update no action;
