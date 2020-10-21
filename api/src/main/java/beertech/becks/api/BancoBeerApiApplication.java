f33280c6615c525829ccd0c72080acda473c5fda
Victor Carvalho <victorhrgc@gmail.com>
2020-10-21 17:11:12 -0300

    Fixing typo

--- api/src/main/java/beertech/becks/api/BancoBeerApiApplication.java
+++ api/src/main/java/beertech/becks/api/BancoBeerApiApplication.java
@@ -3,7 +3,7 @@ package beertech.becks.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplicationgit
+@SpringBootApplication
 public class BancoBeerApiApplication {
 
 	public static void main(String[] args) {
