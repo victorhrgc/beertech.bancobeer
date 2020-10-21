e26128cfa1008042355ff518e02a552151c76946
Efraim Tenorio <efratec20.tenorio@gmail.com>
2020-10-20 21:49:14 -0300

    commit of modify claims to obtain user data

--- api/src/main/java/beertech/becks/api/security/service/JwtService.java
+++ api/src/main/java/beertech/becks/api/security/service/JwtService.java
@@ -13,35 +13,45 @@ import java.util.Date;
 
 @Service
 public class JwtService {
-	@Value("${security.jwt.expiry}")
-	private String expiry;
+    @Value("${security.jwt.expiry}")
+    private String expiry;
 
-	@Value("${security.jwt.key}")
-	private String key;
+    @Value("${security.jwt.key}")
+    private String key;
 
-	public String getToken(User user) {
-		LocalDateTime dateTimeExpiry = LocalDateTime.now().plusMinutes(Long.parseLong(expiry));
+    public String getToken(User user) {
+        LocalDateTime dateTimeExpiry = LocalDateTime.now().plusMinutes(Long.parseLong(expiry));
+        return Jwts.builder().setClaims(getClaimsUser(user)).
+                setSubject(user.getEmail())
+                .setExpiration(Date.from(dateTimeExpiry.atZone(ZoneId.systemDefault()).toInstant()))
+                .signWith(SignatureAlgorithm.HS512, key).compact();
+    }
 
-		return Jwts.builder().setSubject(user.getEmail())
-				.setExpiration(Date.from(dateTimeExpiry.atZone(ZoneId.systemDefault()).toInstant()))
-				.signWith(SignatureAlgorithm.HS512, key).compact();
-	}
+    public boolean isValidToken(String token) {
+        try {
+            Claims claims = getClaims(token);
+            return LocalDateTime.now()
+                    .isBefore(claims.getExpiration().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
-	public boolean isValidToken(String token) {
-		try {
-			Claims claims = getClaims(token);
-			return LocalDateTime.now()
-					.isBefore(claims.getExpiration().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
-		} catch (Exception e) {
-			return false;
-		}
-	}
+    public String getUserLogin(String token) {
+        return getClaims(token).getSubject();
+    }
 
-	public String getUserLogin(String token) {
-		return getClaims(token).getSubject();
-	}
+    private Claims getClaims(String token) {
+        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+    }
 
-	private Claims getClaims(String token) {
-		return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+    private Claims getClaimsUser(User user) {
+		Claims claimsUser = Jwts.claims().setSubject(user.getEmail());
+		claimsUser.put("id", user.getId());
+		claimsUser.put("documentNumber", user.getDocumentNumber());
+		claimsUser.put("email", user.getEmail());
+		claimsUser.put("auth", user.getRole());
+		claimsUser.put("name", user.getName());
+    	return claimsUser;
 	}
 }
