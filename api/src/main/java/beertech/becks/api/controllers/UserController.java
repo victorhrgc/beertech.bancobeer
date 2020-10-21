d135490071875b370731fa5b92aef34351a92a20
AlexSoares <aleexsoarees@gmail.com>
2020-10-20 21:01:33 -0300

    Authorization Swagger

--- api/src/main/java/beertech/becks/api/controllers/UserController.java
+++ api/src/main/java/beertech/becks/api/controllers/UserController.java
@@ -4,6 +4,7 @@ import static beertech.becks.api.constants.Constants.*;
 
 import javax.validation.Valid;
 
+import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,9 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import beertech.becks.api.exception.user.UserAlreadyExistsException;
 import beertech.becks.api.service.UserService;
 import beertech.becks.api.tos.request.UserRequestTO;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/users")
@@ -34,6 +32,7 @@ public class UserController {
             @ApiResponse(code = 400, message = STATUS_400_BAD_REQUEST),
             @ApiResponse(code = 500, message = STATUS_500_INTERNAL_SERVER_ERROR) })
     @PostMapping
+    @ApiOperation(value = "Create user" , authorizations = @Authorization(value = "JWT"))
 	public ResponseEntity<Object> createUser(@Valid @RequestBody UserRequestTO userRequestTO)
 			throws UserAlreadyExistsException {
         log.info("create user");
